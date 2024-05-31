67bec8d7c76c9022778229d8c00bd38ec9dcfde2
trinhkien123 <kien02082004@gmail.com>
2024-05-31 09:12:33 +0700

    add file all

--- src/Views/Client/home.blade.php
+++ src/Views/Client/home.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +8,9 @@
     <title>Home</title>
     <link rel="stylesheet" href="{{ asset('assets/admin/css/style1.css') }}">
 </head>
+
 <body>
     <h1>Welcome {{ $name }} to my website!</h1>
 </body>
+
 </html>
